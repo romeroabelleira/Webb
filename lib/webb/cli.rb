5ee42061d1c3e71b500f6a38e3c105839d7de36a
Juan Romero Abelleira <juan@romeroabelleira.com>
2014-02-03 11:10:22 +0100

    Add proper cli

---
+++ lib/webb/cli.rb
@@ -0,0 +1,36 @@
+require "thor"
+require "thor/actions"
+require "webb"
+require "ipaddr"
+require "typhoeus"
+
+class Webb::CLI < Thor
+  include Thor::Shell
+  include Thor::Actions
+
+  desc "scan ADDRESS", "Scans a single address for responding webservers."
+  def scan(address)
+    request = Typhoeus::Request.new(address, followlocation: true)
+
+    request.on_complete do |response|
+      %x{open http://#{address}} if response.success?
+    end
+    request.run
+  end
+
+  desc "sweep FROM TO", "Sweeps a certain IP range for responding webservers."
+  def sweep( from, to)
+    range = (IPAddr.new(from)..IPAddr.new(to))
+
+    queue = Typhoeus::Hydra.new
+    range.each do |ip|
+      request = Typhoeus::Request.new(ip.to_s, followlocation: true)
+      request.on_complete do |response|
+        %x{open http://#{ip}} if response.success?
+      end
+      queue.queue(request)
+    end
+    queue.run
+  end
+
+end
