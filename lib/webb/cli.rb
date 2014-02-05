c9be39b201f61932e837d41d4bda3e95f340c68a
Juan Romero Abelleira <juan@romeroabelleira.com>
2014-02-05 08:19:41 +0100

    Slice the queue up
    To avoid memory exploding.

--- lib/webb/cli.rb
+++ lib/webb/cli.rb
@@ -23,14 +23,20 @@ class Webb::CLI < Thor
     range = (IPAddr.new(from)..IPAddr.new(to))
 
     queue = Typhoeus::Hydra.new
-    range.each do |ip|
-      request = Typhoeus::Request.new(ip.to_s, followlocation: true)
-      request.on_complete do |response|
-        %x{open http://#{ip}} if response.success?
+    range.each_slice(64) do |slice|
+      puts "#{slice.first.to_s}..#{slice.last .to_s}"
+      slice.each do |ip|
+        request = Typhoeus::Request.new(ip.to_s, followlocation: true, timeout: 2)
+        request.on_complete do |response|
+          if response.success?
+            %x{open http://#{ip.to_s}}
+            puts ip.to_s
+          end
+        end
+        queue.queue(request)
       end
-      queue.queue(request)
+      queue.run
     end
-    queue.run
   end
 
 end
